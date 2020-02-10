06b1f934d9e1ba0b9aae6a905fd17af2b99c3170
Wilczqu <wilk0809@gmail.com>
2020-02-10 21:17:54 +0000

    Created Reg activity

---
+++ app/src/main/java/com/example/witparkingapp/LoginActivity.kt
@@ -0,0 +1,16 @@
+package com.example.witparkingapp
+
+import android.content.res.Configuration
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+class LoginActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+        setContentView(R.layout.login_activity)
+    }
+
+
+
+}
--- app/src/main/java/com/example/witparkingapp/MainActivity.kt
+++ app/src/main/java/com/example/witparkingapp/MainActivity.kt
@@ -1,12 +1,34 @@
 package com.example.witparkingapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+          button_reg.setOnClickListener {
+              val email = email_edittext_reg.text.toString()
+              val password = password_edittext_reg.text.toString()
+
+              Log.d("Mainactivity","email is: " + email)
+              Log.d("Mainactivity","Password is:$password " )
+
+          }
+
+        textView_register.setOnClickListener{
+            Log.d("MainActivity", "Try to show login activity")
+
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+
+        }
+
     }
+
 }
