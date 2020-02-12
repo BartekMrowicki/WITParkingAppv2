a5f6d26d030ddb08f84b3c71c823b7830dda9b5f
BartekMrowicki <wilk0809@gmail.com>
2020-02-12 16:21:52 +0000

    my login and registration are now connected with firebase auth and i can succesfully log in and create user which i know by using log.d and seeing message in the logcat as well as i made some cosmetic changes

--- app/src/main/java/com/example/witparkingapp/LoginActivity.kt
+++ app/src/main/java/com/example/witparkingapp/LoginActivity.kt
@@ -1,16 +1,56 @@
 package com.example.witparkingapp
 
-import android.content.res.Configuration
+import android.graphics.BitmapFactory
 import android.os.Bundle
-import android.os.PersistableBundle
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.graphics.drawable.RoundedBitmapDrawableFactory
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.login_activity.*
 
 class LoginActivity: AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
         setContentView(R.layout.login_activity)
+        val img = BitmapFactory.decodeResource(resources, R.drawable.topicon2)
+        val round = RoundedBitmapDrawableFactory.create(resources,img)
+
+        round.isCircular = true
+        loginimage.setImageDrawable(round)
+
+        button_log.setOnClickListener {
+            performLogin()
+        }
+
+        textView_log.setOnClickListener{
+            finish()
+        }
     }
 
+    private fun performLogin() {
+        val email = email_edittext_log.text.toString()
+        val password = password_edittext_log.text.toString()
 
+        if (email.isEmpty() || password.isEmpty()) {
+            Toast.makeText(this, "Please fill out email/pw.", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener {
+                if (!it.isSuccessful) return@addOnCompleteListener
+
+                Log.d("Login", "Successfully logged in: ${it.result?.user?.uid}")
+            }
+            .addOnFailureListener {
+                Toast.makeText(this, "Failed to log in: ${it.message}", Toast.LENGTH_SHORT).show()
+            }
+    }
 
 }
+
+
+
--- app/src/main/java/com/example/witparkingapp/MainActivity.kt
+++ app/src/main/java/com/example/witparkingapp/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.example.witparkingapp
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.BitmapFactory
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.graphics.drawable.RoundedBitmapDrawableFactory
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -12,23 +16,52 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-          button_reg.setOnClickListener {
-              val email = email_edittext_reg.text.toString()
-              val password = password_edittext_reg.text.toString()
+        val img = BitmapFactory.decodeResource(resources, R.drawable.topicon2)
+        val round = RoundedBitmapDrawableFactory.create(resources, img)
 
-              Log.d("Mainactivity","email is: " + email)
-              Log.d("Mainactivity","Password is:$password " )
+        round.isCircular = true
+        regimage.setImageDrawable(round)
+        button_log.setOnClickListener {
+            performRegister()
+        }
 
-          }
-
-        textView_register.setOnClickListener{
+        textView_log.setOnClickListener {
             Log.d("MainActivity", "Try to show login activity")
 
+            // launch the login activity somehow
             val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
-
         }
 
     }
+
+    private fun performRegister() {
+        val email = email_edittext_log.text.toString()
+        val password = password_edittext_log.text.toString()
+
+        if (email.isEmpty() || password.isEmpty()) {
+            Toast.makeText(this, "Please enter text in email/pw", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        Log.d("MainActivity", "Email is: " + email)
+        Log.d("MainActivity", "Password: $password")
+
+        // Firebase Authentication to create a user with email and password
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener {
+                if (!it.isSuccessful) return@addOnCompleteListener
+
+                // else if successful
+                Log.d("Main", "Successfully created user with uid: ${it.result?.user?.uid}")
+            }
+            .addOnFailureListener {
+                Log.d("Main", "Failed to create user: ${it.message}")
+                Toast.makeText(this, "Failed to create user: ${it.message}", Toast.LENGTH_SHORT)
+                    .show()
+            }
+
+    }
+
 
 }
